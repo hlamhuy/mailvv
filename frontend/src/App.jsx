b8e00ffda4fd620ef6833727b3b9cc8fd9401505
Huy Hoang <hlamhuy@icloud.com>
2024-12-09 00:49:30 -0600

    added Inbox and Config pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Inbox from './components/Inbox';
+import Config from './components/Config';
+
 const App = () => {
     return (
-        <div className='bg-custom-bg min-h-screen text-white'>
-            <div className='bg-blue-700 p-4 justify-center'>
-                <h1 className='text-4xl font-bold'>Tokki Mail</h1>
-            </div>
+        <Router>
+            <div className='bg-custom-bg min-h-screen text-white'>
+                <div className='bg-blue-700 p-4 flex justify-center items-center'>
+                    <h1 className='text-4xl font-bold'>Tokki Mail</h1>
+                </div>
 
-            <nav className='flex space-x-4 bg-nav-bg p-4'>
-                <a href='#inbox' className=' hover:text-blue-700'>
-                    Inbox
-                </a>
-                <a href='#config' className=' hover:text-blue-700'>
-                    Config
-                </a>
-            </nav>
-        </div>
+                <nav className='flex space-x-4 bg-nav-bg p-4'>
+                    <Link to='/' className='hover:text-blue-700'>
+                        Inbox
+                    </Link>
+                    <Link to='/config' className='hover:text-blue-700'>
+                        Config
+                    </Link>
+                </nav>
+
+                <Routes>
+                    <Route path='/' element={<Inbox />} />
+                    <Route path='/config' element={<Config />} />
+                </Routes>
+            </div>
+        </Router>
     );
 };
 
---
+++ frontend/src/components/Inbox.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Inbox = () => {
+    return <div>Inbox Page</div>;
+};
+
+export default Inbox;
