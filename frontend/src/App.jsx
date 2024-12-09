050edf2395b5b2094388a1814ef5897764868e77
Huy Hoang <hlamhuy@icloud.com>
2024-12-09 00:12:07 -0600

    added Tailwind CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 const App = () => {
     return (
-        <div>
-            <p>Hello world</p>
+        <div className='bg-custom-bg min-h-screen text-white'>
+            <div className='bg-blue-700 p-4 justify-center'>
+                <h1 className='text-4xl font-bold'>Tokki Mail</h1>
+            </div>
+
+            <nav className='flex space-x-4 bg-nav-bg p-4'>
+                <a href='#inbox' className=' hover:text-blue-700'>
+                    Inbox
+                </a>
+                <a href='#config' className=' hover:text-blue-700'>
+                    Config
+                </a>
+            </nav>
         </div>
     );
 };
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'custom-bg': '#18191A',
+                'nav-bg': '#5383E8',
+            },
+        },
+    },
+    plugins: [],
+};
