8eb4af8ee021046a7ff414179c89ff4b0aaa0ea9
Huy Hoang <hlamhuy@icloud.com>
2024-12-09 15:24:07 -0600

    added some styles to config page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,15 +10,19 @@ const App = () => {
                 <div className='bg-blue-700 p-4 flex justify-center items-center'>
                     <h1 className='text-4xl font-bold'>Tokki Mail</h1>
                 </div>
-
-                <nav className='flex space-x-4 bg-nav-bg p-4'>
-                    <Link to='/' className='hover:text-blue-700'>
-                        Inbox
-                    </Link>
-                    <Link to='/config' className='hover:text-blue-700'>
-                        Config
-                    </Link>
-                </nav>
+                <div className='block px-60 bg-nav-bg'>
+                    <nav className='flex space-x-8  p-4'>
+                        <Link to='/' className='text-gray-300 hover:text-white'>
+                            Inbox
+                        </Link>
+                        <Link
+                            to='/config'
+                            className='text-gray-300 hover:text-white'
+                        >
+                            Config
+                        </Link>
+                    </nav>
+                </div>
 
                 <Routes>
                     <Route path='/' element={<Inbox />} />
--- frontend/src/components/Config.jsx
+++ frontend/src/components/Config.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const Config = () => {
-    return <div>Config Page</div>;
+    return (
+        <div className='px-60 py-10'>
+            <div className='flex justify-between w-full'>
+                <div className='flex space-x-6'>
+                    <button className='btn bg-orange-500'>All</button>
+                    <button className='btn bg-orange-500'>Gmail</button>
+                    <button className='btn bg-orange-500'>Hotmail</button>
+                    <button className='btn bg-orange-500'>Yahoo</button>
+                    <button className='btn bg-orange-500'>iCloud</button>
+                </div>
+                <div className='flex space-x-6'>
+                    <button className='btn bg-green-500'>Import</button>
+                    <button className='btn bg-purple-600'>Sync</button>
+                </div>
+            </div>
+            <div className='py-6 table w-full'>
+                <table className='mt-4 min-w-full border-separate border-spacing-0 divide-y'>
+                    <thead>
+                        <tr>
+                            <th className='text-left'>Email</th>
+                            <th className='text-left'>Status</th>
+                            <th className='text-left'>Last Sync</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default Config;
