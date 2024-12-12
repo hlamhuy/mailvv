18485eec25c30345bffc6f16fd6ac58d676a2992
Huy Hoang <hlamhuy@icloud.com>
2024-12-11 20:19:05 -0600

    adding some styles to the table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Inbox from './components/Inbox';
 import Config from './components/Config';
 
-
 const App = () => {
     return (
         <Router>
--- frontend/src/components/Config.jsx
+++ frontend/src/components/Config.jsx
@@ -96,40 +96,159 @@ const Config = () => {
                     </button>
                 </div>
             </div>
-            <div className='py-6 table w-full'>
-                <table className='mt-4 min-w-full border-separate border-spacing-0 divide-y'>
-                    <thead>
-                        <tr>
-                            <th className='text-left'>Email</th>
-                            <th className='text-left'>Status</th>
-                            <th className='text-left'>Last Synced</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {accounts.map((account) => (
-                            <tr key={account.id}>
-                                <td>{account.user}</td>
-                                <td
-                                    className={
-                                        account.alive !== null
-                                            ? account.alive
-                                                ? 'text-green'
-                                                : 'text-red'
-                                            : 'text-gray'
-                                    }
-                                >
-                                    {account.alive !== null
-                                        ? account.alive
-                                            ? 'Alive'
-                                            : 'Dead'
-                                        : '-'}
-                                </td>
-                                <td>{account.last_synced || '-'}</td>
+            <div className='pt-10'>
+                <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+                    <table className='w-full text-sm text-left text-gray-300'>
+                        <thead className='text-sm bg-blue-900 text-gray-200'>
+                            <tr>
+                                <th scope='col' className='p-4'>
+                                    <div className='flex items-center'>
+                                        <input
+                                            id='checkbox-all-search'
+                                            type='checkbox'
+                                            className='w-4 h-4'
+                                        />
+                                        <label
+                                            for='checkbox-all-search'
+                                            className='sr-only'
+                                        >
+                                            checkbox
+                                        </label>
+                                    </div>
+                                </th>
+                                <th scope='col' className='px-6 py-3'>
+                                    Email
+                                </th>
+                                <th scope='col' className='px-6 py-3'>
+                                    Status
+                                </th>
+                                <th scope='col' className='px-6 py-3'>
+                                    Last Synced
+                                </th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {accounts.map((account) => (
+                                <tr
+                                    key={account.id}
+                                    className=' odd:bg-gray-800  even:bg-gray-700 border-b border-gray-700'
+                                >
+                                    <td class='w-4 p-4'>
+                                        <div className='flex items-center'>
+                                            <input
+                                                id='checkbox-table-search'
+                                                type='checkbox'
+                                                class='w-4 h-4'
+                                            />
+                                            <label
+                                                for='checkbox-table-search'
+                                                class='sr-only'
+                                            >
+                                                checkbox
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td className='px-6 py-4 text-white'>
+                                        {account.user}
+                                    </td>
+                                    <td
+                                        className={
+                                            account.alive !== null
+                                                ? account.alive
+                                                    ? 'px-6 py-4 text-green'
+                                                    : 'px-6 py-4 text-red'
+                                                : 'px-6 py-4 text-gray'
+                                        }
+                                    >
+                                        {account.alive !== null
+                                            ? account.alive
+                                                ? 'Alive'
+                                                : 'Dead'
+                                            : '-'}
+                                    </td>
+                                    <td className='px-6 py-4'>
+                                        {account.last_synced || '-'}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
+            <nav
+                class='flex items-center flex-column flex-wrap md:flex-row justify-between pt-4'
+                aria-label='Table navigation'
+            >
+                <span class='text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto'>
+                    Showing{' '}
+                    <span class='font-semibold text-gray-900 dark:text-white'>
+                        1-10
+                    </span>{' '}
+                    of{' '}
+                    <span class='font-semibold text-gray-900 dark:text-white'>
+                        1000
+                    </span>
+                </span>
+                <ul class='inline-flex -space-x-px rtl:space-x-reverse text-sm h-8'>
+                    <li>
+                        <a
+                            href='#'
+                            class='flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                        >
+                            Previous
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href='#'
+                            class='flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                        >
+                            1
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href='#'
+                            class='flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                        >
+                            2
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href='#'
+                            aria-current='page'
+                            class='flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
+                        >
+                            3
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href='#'
+                            class='flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                        >
+                            4
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href='#'
+                            class='flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                        >
+                            5
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href='#'
+                            class='flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                        >
+                            Next
+                        </a>
+                    </li>
+                </ul>
+            </nav>
             <Modal
                 isOpen={isModalOpen}
                 onClose={() => setIsModalOpen(false)}
