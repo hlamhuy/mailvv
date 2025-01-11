0fa836efa062bd4a21de844a80e5bbcbaf49d3d4
Huy Hoang <hlamhuy@icloud.com>
2025-01-11 10:00:01 -0600

    change port and make express serve static html file

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-import express, { json } from "express";
-import accountsRouter from "./controllers/accounts.js";
-import mailsRouter from "./controllers/mails.js";
-import initializeDatabase from "./utils/db.js";
+import express, { json } from 'express';
+import accountsRouter from './controllers/accounts.js';
+import mailsRouter from './controllers/mails.js';
+import initializeDatabase from './utils/db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 3000;
+const port = 11125;
 
 initializeDatabase();
 
-app.use(json({ limit: "50mb" }));
-app.use("/api/accounts", accountsRouter);
-app.use("/api/mails", mailsRouter);
+app.use(json({ limit: '50mb' }));
+app.use('/api/accounts', accountsRouter);
+app.use('/api/mails', mailsRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
